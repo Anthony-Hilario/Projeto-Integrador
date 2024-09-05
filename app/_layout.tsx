eedd812ff75bad9344fecebc5f8ea1e2b29d71d5
Anthony Hilario <137835837+Anthony-Hilario@users.noreply.github.com>
2024-09-05 11:43:19 -0300

    server mqtt

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome';
-
 import Index from './index';
 import LogPageScreen from './logPage';
-import QRCodeScreen from './qrcode';
 import Configs from './configs';
+import QRCodeGenerator from './qrcode';
+
+// Define os estilos para o título personalizado
+const styles = StyleSheet.create({
+  img: {
+    width: 40,  // Ajuste o tamanho conforme necessário
+    height: 40,
+    marginLeft: 10, // Ajusta o espaçamento à esquerda da imagem
+  },
+  headerTitleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  headerTitleText: {
+    fontSize: 24,
+    color: '#fff',
+  },
+});
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -26,11 +43,11 @@ function AppTabs() {
             case 'logPage':
               iconName = 'file-text';
               break;
-            case 'qrcode':
-              iconName = 'qrcode';
-              break;
             case 'config':
               iconName = 'gear';
+              break;
+            case 'qrcode':
+              iconName = 'qrcode';
               break;
             default:
               iconName = 'gear';
@@ -38,23 +55,35 @@ function AppTabs() {
 
           return <Icon name={iconName} size={size} color={color} />;
         },
+        tabBarActiveTintColor: '#fff', // Cor para o ícone ativo
+        tabBarInactiveTintColor: 'gray', // Cor para o ícone inativo
+        tabBarStyle: {
+          backgroundColor: '#E91D63', // Cor de fundo da tab bar
+          paddingBottom: 5
+        },
       })}
     >
-      <Tab.Screen name='home' component={Index} options={{ title: 'Home' }} />
-      <Tab.Screen name="qrcode" component={QRCodeScreen} options={{ title: 'QR Code' }} />
-      <Tab.Screen name="logPage" component={LogPageScreen} options={{ title: 'Logs' }} />
-      <Tab.Screen name='configs'component={Configs} options={{ title: 'Config' }} />
+      <Tab.Screen name='home' component={Index} options={{ title: 'Home', headerShown: false}} />
+      <Tab.Screen name='qrcode' component={QRCodeGenerator} options={{ title: 'QR Code', headerShown: false}} />
+      <Tab.Screen name="logPage" component={LogPageScreen} options={{ title: 'Logs', headerShown: false }} />
+      <Tab.Screen name='configs' component={Configs} options={{ title: 'Config', headerShown: false }} />
     </Tab.Navigator>
   );
 }
 
 function MainStack() {
   return (
-    <Stack.Navigator initialRouteName="Index">
+    <Stack.Navigator initialRouteName="SafeDoor">
       <Stack.Screen 
-        name="Main" 
+        name="SafeDoor"
         component={AppTabs}
-        options={{ headerShown: false }} 
+        options={{
+          headerShown: true,
+          headerStyle: {
+            backgroundColor: '#E91D63', // Cor de fundo da barra superior
+          },
+          headerTintColor: '#fff', // Cor do título na barra superior
+        }} 
       />
     </Stack.Navigator>
   );
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,58 @@
 import { StatusBar, Text, View, Image, Button } from "react-native";
 import { styles } from "@/styles/styles";
+import React, { useEffect, useState } from 'react';
+
+const SERVER_URL = 'ws://localhost:3000'; 
 
 export default function Index() {
+
+  const [ws, setWs] = useState(null);
+  const [message, setMessage] = useState('');
+
+
+  useEffect(() => {
+    // Conecta ao WebSocket do servidor
+    const socket = new WebSocket(SERVER_URL);
+
+    socket.onopen = () => {
+      console.log('Conectado ao WebSocket do servidor');
+    };
+
+    socket.onmessage = (event) => {
+      console.log('Mensagem recebida do servidor:', event.data);
+      setMessage(event.data); // Armazena a mensagem recebida para exibição
+    };
+
+    socket.onerror = (error) => {
+      console.error('Erro no WebSocket:', error.message);
+    };
+
+    socket.onclose = (event) => {
+      console.log('WebSocket desconectado:', event);
+    };
+
+    setWs(socket);
+
+    // Limpa a conexão WebSocket ao desmontar o componente
+    return () => {
+      if (socket) {
+        socket.close();
+      }
+    };
+  }, []);
+
+  const enviarLiberado = () => {
+    if (ws) {
+      ws.send('Liberado');
+    }
+  };
+
+  const enviarBloqueado = () => {
+    if (ws) {
+      ws.send('Bloqueado');
+    }
+  };
+
   return (
   <View style={styles.container}>
 
@@ -11,8 +62,8 @@ export default function Index() {
     </View>
 
     <View style={styles.row}>
-      <Button title="Liberar"/>
-      <Button title="Bloquear"/>
+      <Button title="Liberar" onPress={enviarLiberado}/>
+      <Button title="Bloquear" onPress={enviarBloqueado}/>
     </View>
 
     <StatusBar barStyle={'dark-content'} />
